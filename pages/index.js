ef04060f6630b7eb5998e686ab362fe608c2fdf8
oirase <cashewnutmuffin@outlook.com>
2020-08-13 12:01:52 +0900

    all

--- pages/index.js
+++ pages/index.js
@@ -20,18 +20,18 @@ export default function Home({ allPotsData }) {
         <title>{siteTitle}</title>
       </Head>
        <section className={utilStyles.headingMd}>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-        {allPostsData.map(({ id, date, title }) => {
-          <li className={utilStyles.listItem} key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {date}
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
             </li>
-        })}
+          ))}
         </ul>
       </section>
     </Layout>
